16a53ae5b9605a8c709e5b72f3c82cac29082f45
KizokuHd <Sterpinal@gmail.com>
2021-05-12 21:55:13 -0400

    AvanceVr

---
+++ I have no time VR/Assets/01_IHaveNoTimeVr/02_Scripts/PAto/MuroRomper.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuroRomper : MonoBehaviour
+{
+    public GameObject muroFragmentos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag=="Hand")
+        {
+            Instantiate(muroFragmentos, transform.position, Quaternion.identity);
+            Destroy(this.gameObject);
+        }
+    }
+}
